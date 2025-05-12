9509d2dd8c34db7ffc48b71af17a347e3eaa3a5b
yorozc <143294600+yorozc@users.noreply.github.com>
2025-05-12 15:48:57 -0700

    got the elements I will need for calculating weight, created weight calculator func

--- fileProcessor.py
+++ fileProcessor.py
@@ -2,15 +2,34 @@ class fileprocessor:
 
     def __init__(self, file):
         self.file = file
+        self.weights = {
+            "HW": 0.25,
+            "QUIZ": 0.25,
+            "EXAM": 0.35,
+            "PROJECT": 0.15
+        }
 
     def processFile(self): #parses file
         try:
-            with open(self.file) as file:
-                print(file.read())
+            with open(self.file, 'r') as file:
+                for line in file:
+                    #print(line, end="")
+                    gradeList = line.split() #makes string an array
+                    category = gradeList[0].upper()
+                    grades = gradeList[1:]
+                    print(grades)
+                    if category in self.weights:
+                        print(self.weights[category])
+                #separate first characters up until first white space
+            
+
         except FileNotFoundError:
             print("FILE NOT FOUND")
 
     def createsResult(self): #creates result.txt
+        pass
+
+    def calculateWeights(self):
         pass
 
     def processAndResults(self):
--- main.py
+++ main.py
@@ -5,7 +5,6 @@ class Main:
     def main():
         gradeFile = input("Please insert grade file: ")
         gradeResult = fileprocessor(gradeFile) #passes string
-        #gradeResult.processFile()
         gradeResult.processAndResults()
 
     if __name__ == "__main__":
