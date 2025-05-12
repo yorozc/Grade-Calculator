6333c27a7daed8e2329741217ba2e87321cf3eab
yorozc <143294600+yorozc@users.noreply.github.com>
2025-05-12 14:23:49 -0700

    moved both processfile and createsresult to a wrapper function called processAndResults()

--- fileProcessor.py
+++ fileProcessor.py
@@ -3,12 +3,13 @@ class fileprocessor:
     def __init__(self, file):
         self.file = file
 
-    def processFile(self):
+    def processFile(self): #parses file
         with open(self.file) as file:
             print(file.read())
 
-    def createsResult(self):
+    def createsResult(self): #creates result.txt
         pass
 
-    def operations(self):
-        pass
+    def processAndResults(self):
+        self.processFile()
+        self.createsResult()
--- main.py
+++ main.py
@@ -5,7 +5,8 @@ class Main:
     def main():
         gradeFile = input("Please insert grade file: ")
         gradeResult = fileprocessor(gradeFile) #passes string
-        gradeResult.processFile()
+        #gradeResult.processFile()
+        gradeResult.operations()
 
     if __name__ == "__main__":
         main()
