37b89337f4b48617aeb9082dc77c2549b381eca0
yorozc <143294600+yorozc@users.noreply.github.com>
2025-05-20 15:00:14 -0700

    added window title and window icon, new pictures folder created to hold images that will be used in the project

--- mainWindow.py
+++ mainWindow.py
@@ -1,2 +1,12 @@
 from PySide6 import QtCore, QtWidgets, QtGui
-from PySide6.QtWidgets import QMainWindow, QVBoxLayout, QPushButton, QLabel, QWidget
+from PySide6.QtWidgets import (QMainWindow, QVBoxLayout, QPushButton, QLabel, QWidget,
+                               )
+
+from PySide6.QtGui import QIcon, QFont, QPixmap
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setGeometry(600, 150, 750, 750)
+        self.setWindowTitle("CS3750 Grade Calculator")
+        self.setWindowIcon(QIcon("pictures/cartoon-notebook-icon-png.png"))
